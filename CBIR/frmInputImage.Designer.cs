4e43536b729c63930859422dbe7ae8b5bb227c23
unknown <tasawernawaz@gmail.com>
2014-11-09 22:54:42 +0500

    initial commit

---
+++ CBIR/frmInputImage.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CBIR
+{
+    partial class frmInputImage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowes = new System.Windows.Forms.Button();
+            this.pbINewImage = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbINewImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowes
+            // 
+            this.btnBrowes.Location = new System.Drawing.Point(108, 290);
+            this.btnBrowes.Name = "btnBrowes";
+            this.btnBrowes.Size = new System.Drawing.Size(101, 23);
+            this.btnBrowes.TabIndex = 0;
+            this.btnBrowes.Text = "Browes Image";
+            this.btnBrowes.UseVisualStyleBackColor = true;
+            this.btnBrowes.Click += new System.EventHandler(this.btnBrowes_Click);
+            // 
+            // pbINewImage
+            // 
+            this.pbINewImage.Location = new System.Drawing.Point(108, 58);
+            this.pbINewImage.Name = "pbINewImage";
+            this.pbINewImage.Size = new System.Drawing.Size(182, 209);
+            this.pbINewImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbINewImage.TabIndex = 1;
+            this.pbINewImage.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(215, 290);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // frmInputImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 472);
+            this.Controls.Add(this.pbINewImage);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnBrowes);
+            this.Name = "frmInputImage";
+            this.Text = "Insert Image";
+            ((System.ComponentModel.ISupportInitialize)(this.pbINewImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowes;
+        private System.Windows.Forms.PictureBox pbINewImage;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ CBIR/frmInputImage.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AForge.Imaging;
+
+namespace CBIR
+{
+    public partial class frmInputImage : Form
+    {
+        public frmInputImage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnBrowes_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFD = new OpenFileDialog();
+            DialogResult dlr = oFD.ShowDialog();
+            if (dlr != DialogResult.Cancel)
+            {
+                pbINewImage.ImageLocation = oFD.FileName;
+            }
+        }
+
+        private void ConvertImgetoHistrograms(string imgLocation)
+        {
+            System.Drawing.Bitmap picture = new System.Drawing.Bitmap(imgLocation);
+            // Luminance
+            long[] myHistogram1 = new long[256];
+
+            for (int i = 0; i < picture.Size.Width; i++)
+                for (int j = 0; j < picture.Size.Height; j++)
+                {
+                    System.Drawing.Color c = picture.GetPixel(i, j);
+
+                    long Temp = 0;
+                    Temp += c.R;
+                    Temp += c.G;
+                    Temp += c.B;
+
+                    Temp = (int)Temp / 3;
+                    myHistogram1[Temp]++;
+                }
+
+            
+            //image 2
+            String image2 = @"C:\Users\Tasawer Nawaz\Desktop\1235.jpg";
+            long[] myHistogram2 = new long[256];
+            System.Drawing.Bitmap picture1 = new System.Drawing.Bitmap(image2);
+            // Luminance
+            for (int i = 0; i < picture1.Size.Width; i++)
+                for (int j = 0; j < picture1.Size.Height; j++)
+                {
+                    System.Drawing.Color c = picture1.GetPixel(i, j);
+
+                    long Temp = 0;
+                    Temp += c.R;
+                    Temp += c.G;
+                    Temp += c.B;
+
+                    Temp = (int)Temp / 3;
+                    myHistogram2[Temp]++;
+                }
+
+            // meauring difference
+
+            float[] diff = new float[256];
+            for (int i = 0; i < 255; i++)
+            {
+                diff[i] = ((float)Math.Abs(myHistogram1[i] - myHistogram2[i])) / 255;
+            }
+
+            float degreeOfDiff = diff.Sum() / myHistogram1.Length * 100;
+            bool sameDistribution = degreeOfDiff == 0;
+
+            MessageBox.Show(degreeOfDiff + "%");
+            //MessageBox.MessageBox.Show(sameDistribution.ToString());
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            ConvertImgetoHistrograms(pbINewImage.ImageLocation);
+        }
+    }
+}
